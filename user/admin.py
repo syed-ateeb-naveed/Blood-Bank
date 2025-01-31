ea030a3793f7d270fd1ee553cf3bfe44868ee43e
ateeb <ateebnaveed1996@gmail.com>
2025-02-01 01:01:44 +0500

    Admin view modified

--- user/admin.py
+++ user/admin.py
@@ -3,4 +3,19 @@ from .models import User
 
 # Register your models here.
 
-admin.site.register(User)
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    """
+    Admin configuration for the User model.
+    """
+    list_display = ['first_name', 'email', 'date_of_birth', 'age']
+
+    search_fields = ['first_name', 'last_name', 'email', 'date_of_birth', 'age']
+
+    list_filter = ['created_at', 'modified_at', 'is_active', 'is_staff']
+
+    readonly_fields = ['created_at', 'modified_at']
+
+    ordering = ['-created_at']
+
+    list_per_page = 20
