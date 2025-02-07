ca2bce9c8adf5158a927a70279a7bd05c020f0f3
ateeb <ateebnaveed1996@gmail.com>
2025-02-07 22:07:42 +0500

    donor model

--- donor/models.py
+++ donor/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Donor(models.Model):
+
+    user = models.OneToOneField('user.User', on_delete=models.CASCADE, related_name='donor')
+    blood_group = models.CharField(choices=[('A+', 'A+'), ('A-', 'A-'), ('B+', 'B+'), ('B-', 'B-'), ('AB+', 'AB+'), ('AB-', 'AB-'), ('O+', 'O+'), ('O-', 'O-')], max_length=3, blank=True, null=True)
+    # genotype = models.CharField(max_length=255, blank=True, null=True)
+    gender = models.CharField(choices=[('male', 'Male'), ('female', 'Female')])
+    height = models.FloatField(blank=True, null=True)
+    weight = models.FloatField(blank=True, null=True)
+    ailments = models.TextField(blank=True, null=True)
