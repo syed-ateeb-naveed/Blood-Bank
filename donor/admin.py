3fba43aaa0256f37867f2fdb7b146dadbced1e6c
ateeb <ateebnaveed1996@gmail.com>
2025-02-08 20:06:55 +0500

    Admin page

--- donor/admin.py
+++ donor/admin.py
@@ -1,5 +1,27 @@
 from django.contrib import admin
-from .models import Donor
+from .models import Donor, Donation
 # Register your models here.
 
-admin.site.register(Donor)
+@admin.register(Donor)
+class DonorAdmin(admin.ModelAdmin):
+    """
+    Admin configuration for the Donor model.
+    """
+    list_display = ['get_donor_object', 'blood_group', 'get_user_email']
+
+    def get_user_email(self, obj):
+        return obj.user.email
+    get_user_email.short_description = 'Email'
+
+    def get_donor_object(self, obj):
+        return obj
+    get_donor_object.short_description = 'Donor'
+
+@admin.register(Donation)
+class DonationAdmin(admin.ModelAdmin):
+    
+    list_display = ['donor', 'date', 'time', 'units', 'location']
+    # list_filter = ['date', 'location']
+    search_fields = ['donor__user__email', 'location']
+    ordering = ['-date', '-time']
+    list_per_page = 20
