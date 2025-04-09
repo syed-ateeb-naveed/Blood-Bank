0dec0de4dcd07809601645e63995a17a5c712adf
ateeb <ateebnaveed1996@gmail.com>
2025-04-09 10:24:15 +0500

    Patient and Request models and their admin views

--- backend/settings.py
+++ backend/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt.token_blacklist',
     'user',
     'donor',
+    'patient',
 ]
 
 AUTH_USER_MODEL = 'user.User'
---
+++ patient/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+import datetime
+
+# Create your models here.
+
+
+class Patient(models.Model):
+    
+    user = models.OneToOneField('user.User', on_delete=models.CASCADE, related_name='patient')
+
+    def __str__(self):
+        return self.user.first_name + ' ' + self.user.last_name
+
+class Request(models.Model):
+
+    blood_type =  models.CharField(choices=[('A+', 'A+'), ('A-', 'A-'), ('B+', 'B+'), ('B-', 'B-'), ('AB+', 'AB+'), ('AB-', 'AB-'), ('O+', 'O+'), ('O-', 'O-')], max_length=3)
+    request_date = models.DateField(default=datetime.date.today)
+    status = models.CharField(choices=[('pending', 'Pending'), ('fulfilled', 'Fulfilled'), ('cancelled', 'Cancelled')], max_length=10, default='pending')
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE, related_name='requests')
+
+    def __str__(self):
+        return f"{self.patient.user.first_name} {self.patient.user.last_name} - {self.blood_type} - {self.units_required} - {self.request_date}"
