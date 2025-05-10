4cd1033eee8de1efcc0e23bd3d76a7f61222aa89
ateeb <ateebnaveed1996@gmail.com>
2025-05-11 03:56:38 +0500

    CSRF

--- backend/settings.py
+++ backend/settings.py
@@ -32,13 +32,11 @@ DEBUG = True
 
 # ALLOWED_HOSTS = ["*"]
 ALLOWED_HOSTS = [
-    "blood-bank-production-60a7.up.railway.app",
-    "localhost",
-    "127.0.0.1",
+    "*"
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    "blood-bank-production-60a7.up.railway.app",
+    "*"
 ]
 # Application definition
 
