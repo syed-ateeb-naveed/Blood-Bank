a9c22369427d213429e32ca27c632ce7f8c95d75
ateeb <ateebnaveed1996@gmail.com>
2025-05-11 03:31:56 +0500

    import os

--- backend/settings.py
+++ backend/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 from datetime import timedelta
 from dotenv import load_dotenv
-
+import os
 # point to the .env file
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
